4b0584d7db8a1f7b043c6dc2991acbf77e752c33
“ILARIO-KAGOO” <ilariokagoo5047.sse@saveetha.com>
2023-08-17 12:33:18 +0530

    QN-3

--- Decision_tree.py
+++ Decision_tree.py
@@ -1,76 +1,121 @@
-import pandas as pd
-import numpy as np
 import math
+import csv
+def load_csv(filename):
+    lines=csv.reader(open(filename,"r"));
+    dataset = list(lines)
+    headers = dataset.pop(0)
+    return dataset,headers
 
-# Define a class for the decision tree node
-class DecisionTreeNode:
-    def __init__(self, attribute=None, label=None, branches={}):
-        self.attribute = attribute  # the attribute used to split the data
-        self.label = label  # the label assigned to this node
-        self.branches = branches  # the branches of the decision tree
+class Node:
+    def __init__(self,attribute):
+        self.attribute=attribute
+        self.children=[]
+        self.answer=""
+        
+def subtables(data,col,delete):
+    dic={}
+    coldata=[row[col] for row in data]
+    attr=list(set(coldata))
+    
+    counts=[0]*len(attr)
+    r=len(data)
+    c=len(data[0])
+    for x in range(len(attr)):
+        for y in range(r):
+            if data[y][col]==attr[x]:
+                counts[x]+=1
+        
+    for x in range(len(attr)):
+        dic[attr[x]]=[[0 for i in range(c)] for j in range(counts[x])]
+        pos=0
+        for y in range(r):
+            if data[y][col]==attr[x]:
+                if delete:
+                    del data[y][col]
+                dic[attr[x]][pos]=data[y]
+                pos+=1
+    return attr,dic
+    
+def entropy(S):
+    attr=list(set(S))
+    if len(attr)==1:
+        return 0
+    
+    counts=[0,0]
+    for i in range(2):
+        counts[i]=sum([1 for x in S if attr[i]==x])/(len(S)*1.0)
+    
+    sums=0
+    for cnt in counts:
+        sums+=-1*cnt*math.log(cnt,2)
+    return sums
 
-# Define a function to calculate the entropy of a dataset
-def entropy(data):
-    target = data['target']
-    n = len(target)
-    unique, counts = np.unique(target, return_counts=True)
-    entropy = 0
-    for i in range(len(unique)):
-        p = counts[i] / n
-        entropy -= p * math.log2(p)
-    return entropy
+def compute_gain(data,col):
+    attr,dic = subtables(data,col,delete=False)
+    
+    total_size=len(data)
+    entropies=[0]*len(attr)
+    ratio=[0]*len(attr)
+    
+    total_entropy=entropy([row[-1] for row in data])
+    for x in range(len(attr)):
+        ratio[x]=len(dic[attr[x]])/(total_size*1.0)
+        entropies[x]=entropy([row[-1] for row in dic[attr[x]]])
+        total_entropy-=ratio[x]*entropies[x]
+    return total_entropy
 
-# Define a function to calculate the information gain of an attribute
-def information_gain(data, attribute):
-    n = len(data)
-    values = data[attribute].unique()
-    entropy_s = entropy(data)
-    entropy_attr = 0
-    for value in values:
-        subset = data[data[attribute] == value]
-        subset_n = len(subset)
-        subset_entropy = entropy(subset)
-        entropy_attr += subset_n / n * subset_entropy
-    return entropy_s - entropy_attr
+def build_tree(data,features):
+    lastcol=[row[-1] for row in data]
+    if(len(set(lastcol)))==1:
+        node=Node("")
+        node.answer=lastcol[0]
+        return node
+    
+    n=len(data[0])-1
+    gains=[0]*n
+    for col in range(n):
+        gains[col]=compute_gain(data,col)
+    split=gains.index(max(gains))
+    node=Node(features[split])
+    fea = features[:split]+features[split+1:]
 
-# Define the ID3 algorithm
-def id3(data, attributes):
-    target = data['target']
-    # If all the examples have the same target value, return a leaf node with that value
-    if len(target.unique()) == 1:
-        return DecisionTreeNode(label=target.iloc[0])
-    # If there are no attributes left to split on, return a leaf node with the most common target value
-    if len(attributes) == 0:
-        return DecisionTreeNode(label=target.value_counts().idxmax())
-    # Otherwise, select the attribute with the highest information gain
-    gains = {attr: information_gain(data, attr) for attr in attributes}
-    best_attribute = max(gains, key=gains.get)
-    # Create a new decision tree node with the selected attribute
-    node = DecisionTreeNode(attribute=best_attribute)
-    # Split the data based on the selected attribute and recursively build the tree
-    for value in data[best_attribute].unique():
-        subset = data[data[best_attribute] == value].drop(best_attribute, axis=1)
-        if len(subset) == 0:
-            node.branches[value] = DecisionTreeNode(label=target.value_counts().idxmax())
-        else:
-            new_attributes = attributes.copy()
-            new_attributes.remove(best_attribute)
-            node.branches[value] = id3(subset, new_attributes)
+    
+    attr,dic=subtables(data,split,delete=True)
+    
+    for x in range(len(attr)):
+        child=build_tree(dic[attr[x]],fea)
+        node.children.append((attr[x],child))
     return node
 
-# Load the dataset
-data = pd.read_csv("Datasets\PlayTennis.csv")
-# Split the dataset into attributes and target variable
-attributes = data.columns[:-1].tolist()
-# Build the decision tree using ID3 algorithm
-root = id3(data, attributes)
+def print_tree(node,level):
+    if node.answer!="":
+        print("  "*level,node.answer)
+        return
+    
+    print("  "*level,node.attribute)
+    for value,n in node.children:
+        print("  "*(level+1),value)
+        print_tree(n,level+2)
 
-# Define a function to classify a new sample using the decision tree
-def classify(sample, tree):
-    if tree.label is not None:
-        return tree.label
-    attribute = tree.attribute
-    value = sample[attribute]
-    if value not in tree.branches:
-        return tree.branches[max(tree.branches.keys(), key=int)]
-    subtree = tree
+        
+def classify(node,x_test,features):
+    if node.answer!="":
+        print(node.answer)
+        return
+    pos=features.index(node.attribute)
+    for value, n in node.children:
+        if x_test[pos]==value:
+            classify(n,x_test,features)
+            
+# Main Program
+dataset,features=load_csv("Datasets\id3.csv")
+node1=build_tree(dataset,features)
+
+print("The decision tree for the dataset using ID3 algorithm is")
+print_tree(node1,0)
+testdata,features=load_csv("Datasets\id3_test.csv")
+
+for xtest in testdata:
+    print("The test instance:",xtest)
+    print("The label for test instance:",end="   ")
+    classify(node1,xtest,features)
